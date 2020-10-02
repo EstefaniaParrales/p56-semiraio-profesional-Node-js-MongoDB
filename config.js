604466e19c112f0998e3932d4adfbbcc0f539490
Estefania Parrales <estefaniak97@hotmail.com>
2020-10-02 04:46:33 -0500

    Proyecto de Seminario

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://adminUPS:ups2020@cluster0.dclfj.gcp.mongodb.net/<database>?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:2020ups@cluster0.jacjb.gcp.mongodb.net/<BaseDeDatos>?retryWrites=true&w=majority',
     port: process.env.PORT || 5000,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/',
